c7ab5a2d4afc4e7ae53fe2e0b509ce51657d8c40
Felipe Álvarez <luisfelipealvarez@gmail.com>
2015-04-09 23:57:35 -0300

    Updating travis

--- runtests.py
+++ runtests.py
@@ -5,6 +5,6 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testing_settings")
 
-    from django.core.management import execute_from_command_line
+    from django.core.management import call_command
 
-    execute_from_command_line(sys.argv)
+    call_command("test")
