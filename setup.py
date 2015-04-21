49abd36d49fde448a874fae3745b1396afca4d7a
Felipe Álvarez <falvarez@votainteligente.cl>
2015-04-21 12:36:08 -0300

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name = 'ddah-promises',
-    version = '0.1',
+    version = '0.1.1',
     packages = ['promises'],
     include_package_data = True,
     license = 'Affero',
